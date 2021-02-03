266d7e611e9c1f6fbe2e9eeb1491d8bf8134bbfb
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-02-03 16:03:28 -0500

    Create controller#actions

---
+++ Routes/app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+    def index
+        # users = User.all 
+        # render json: users
+        # render plain: "Hammad$ Hammad$ Hammad$ Hammad$ Hammad$ Hammad$ Hammad$ "
+        render json: params
+    end
+
+    def show
+        render plain: "#{params}"
+    end
+
+    def create
+        render json: params
+    end
+
+    def update
+        render json: params
+    end
+
+    # def create
+    #     user = User.new(user_params)
+    #     if user.save
+    #         render json: user
+    #     else
+    #         render json: user.errors.full_messages, status: 418
+    #     end
+    # end
+
+    # def new
+    # end
+
+    # def user_params
+    #     params #require().permit()
+    # end
+
+end
